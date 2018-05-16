85a1eff8c2a010e617f5da2faa03ca4afc28e1ec
Admin <rameshkror@gmail.com>
2018-05-16 14:34:46 +0530

    React ChatBot

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to TVS LsL ChatBot</h1>
+        </header>
+        <img src="https://www.0grados.com/admin/wp-content/uploads/2016/08/Neg-El-valor-de-la-logistica.jpg" alt=""/>
+      </div>
+    );
+  }
+}
+
+export default App;
